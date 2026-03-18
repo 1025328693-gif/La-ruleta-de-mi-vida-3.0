a9ef8990eb736f65848fb3cf8cb70fc016916559
Jero <1025328693@alumnos.innovaschools.edu.pe>
2026-03-17 19:37:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+Jero = ["Leer", "Jugar con mi hermano", "Jugar en mi playstation", "Ver tele", "Jugar en mi computador" Jugar basket ball", "Jugara futball"]
+print(random.choice(Jero))
