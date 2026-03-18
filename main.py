962b2ce792d271f6684a858f598557d4fd7a4048
Jero <1025328693@alumnos.innovaschools.edu.pe>
2026-03-18 18:02:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import random
-Jero = ["Leer", "Jugar con mi hermano", "Jugar en mi playstation", "Ver tele", "Jugar en mi computador" Jugar basket ball", "Jugara futball"]
+Jero = ["Leer", "Jugar con mi hermano", "Jugar en mi playstation", "Ver tele", "Jugar en mi computador" Jugar basket ball", "Jugara futball", "Jugar bolos", "Ver pelis"]
 print(random.choice(Jero))
